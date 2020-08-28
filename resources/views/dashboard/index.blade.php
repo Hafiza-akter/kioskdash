53e9e939a2bac9c4cd2413276e164c29be0a5a26
biplop hossain <arnobsec21@gmail.com>
2020-08-29 00:42:56 +0600

    slider timer reduce

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -94,8 +94,8 @@ video {
 <body>
 
 
-<div class="container-fluid px-0"> <!-- data-interval="true" -->
-    <div id="carouselExampleControls" class="carousel slide"  data-pause="hover"  data-ride="carousel">
+<div class="container-fluid px-0"> <!-- data-interval="true" data-pause="hover"-->
+    <div id="carouselExampleControls" class="carousel slide"    data-ride="carousel">
     {{-- <div id="carouselExampleControls" class="carousel slide"  data-interval="false"  data-ride="carousel"> --}}
         <div class="carousel-inner bg-info" role="listbox">
           @include('dashboard.components.sliders.kiosk_01_welcome')
