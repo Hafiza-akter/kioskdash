2478d4c09fb4d3b73da4af3138cd59e92a6f0325
biplop hossain <arnobsec21@gmail.com>
2020-09-07 17:59:18 +0600

    background images changes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -233,7 +233,7 @@ svg {
 <script>
 currentTime();
 
-  var mymap = L.map('mapid').setView([23.777176,  90.399452], 7);
+  var mymap = L.map('mapid').setView([23.777176,  90.3563], 7);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
