a5eaeb7d0782c31a65d94cc691ade2469d3ddb7f
biplop hossain <arnobsec21@gmail.com>
2020-09-14 17:29:31 +0600

    css solved

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,19 +11,19 @@
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
 
-
+{{-- 
   <link rel="stylesheet" href="{{ asset('css/leaflet.css')}}"
    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-   crossorigin=""/>
+   crossorigin=""/> --}}
 
     <!-- Make sure you put this AFTER Leaflet's CSS -->
-  <script src="{{asset('js/leaflet.js')}}"
+ {{--  <script src="{{asset('js/leaflet.js')}}"
    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-   crossorigin=""></script>
+   crossorigin=""></script> --}}
 
-{{--   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
- --}}
+
 
 <link rel="stylesheet" type="text/css" href="{{asset('css/rain_master_normalize.css')}}" />
 <link href='{{asset('css/css.css')}}' rel='stylesheet' type='text/css'>
@@ -224,7 +224,6 @@ function updateTime(k) {
 
   return val;
 }
-
 var width = 100,
     perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
     EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
