28aa10b83b3fdfe2fc0234fc1041f08b3938b96f
biplop hossain <arnobsec21@gmail.com>
2020-11-08 17:47:28 +0600

    5 days forcast add

--- resources/views/dashboard/components/sliders/kiosk_slider_11.blade.php
+++ resources/views/dashboard/components/sliders/kiosk_slider_11.blade.php
@@ -1,4 +1,4 @@
-<div class="carousel-item {{ $key == 0 ? 'active' : ''}}" data-interval="{{ $data['duration'] ? $data['duration'] * 1000 : 6000}}" id="kiosk_05_tendaysforcast">
+<div class="carousel-item {{ $key == 0 ? 'active' : ''}}" data-interval="{{ $data['duration'] ? $data['duration'] * 1000 : 6000}}" id="kiosk_11_fivedaysforcast">
 
 
   <div class="d-flex align-items-center justify-content-center">
@@ -11,10 +11,10 @@
               </div>
 
             <div style="height: 6vh;width:auto;margin-top:1.8%">
-               <p class="heading_center">10 days forecast</p>
+               <p class="heading_center">5 days forecast</p>
               </div>
               {{-- http://www.ffwc.gov.bd/ffwc_charts/index.php?stid=93 --}}
-          <iframe id="10-days-for" src="http://www.ffwc.gov.bd/medium_range/index.php?stid={{ $data['station_id']}}" style="pointer-events: none;width:80%; height:80vh; border:none; margin-top:10px; padding:0; overflow:hidden; z-index:999999;">
+          <iframe id="05-days-for" src="http://www.ffwc.gov.bd/ffwc_charts/index.php?stid={{ $data['station_id']}}" style="pointer-events: none;width:80%; height:80vh; border:none; margin-top:10px; padding:0; overflow:hidden; z-index:999999;">
           Your browser doesn't support iframes
           </iframe>
         </div>
