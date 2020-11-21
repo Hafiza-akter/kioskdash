4713dbe0c7d30e539e3df562aa641511fa87e0d9
biplop hossain <arnobsec21@gmail.com>
2020-11-22 00:08:08 +0600

    logo added to footer

--- resources/views/dashboard/components/sliders/common/slider_footer.blade.php
+++ resources/views/dashboard/components/sliders/common/slider_footer.blade.php
@@ -1,68 +1,30 @@
 <footer style="height: 7%;position: fixed;bottom:0;left: 0;width:100%;">
-        <div class="top-footer-bar">
-        <div class="container">
-            <nav class="footer-menu">
-                {{-- <ul>
-                    <li><a href="https://eksheba.gov.bd/cooperation">সার্বিক সহযোগিতায়</a></li>
-                    <li class="divider"></li>
-                    <li><a href="https://eksheba.gov.bd/privacy-policy">গোপনীয়তার নীতিমালা</a></li>
-                    <li class="divider"></li>
-                    <li><a href="https://eksheba.gov.bd/usage-conditions">ব্যবহারের শর্তাবলি</a></li>
-                    <li class="divider"></li>
-                    <li><a data-toggle="modal" style="text-transform:uppercase;" data-target="#faq"> সচরাচর জিজ্ঞাসা</a></li>
-                    <li class="divider"></li>
-                    <li><a data-toggle="modal" data-target="#helpdesk-modal">হেল্প ডেস্ক</a></li>
-                                        
-                </ul> --}}
-            </nav>
-        </div>
-    </div>
-    <div class="container footer">
-        <div class="row">
-            <div class="col-12 text-center">
-                                <div class="">
-                    <div class="credit">
-                        {{-- <div class="credit-title">
-                            Powered by                     
-                        </div> --}}
-
-                        
-                        <div class="inline-block relative">
-                            <p>Powered By RIMES</p>
-
-                        </div>
-                        <!--<img class="credit-img" src="img/a2i-logo.png"/>-->
-                    </div>
-                </div>
+    <div class="top-footer-bar">
+            <div class="container">
+                <nav class="footer-menu">
+                    {{-- <ul>
+                        <li><a href="https://eksheba.gov.bd/cooperation">সার্বিক সহযোগিতায়</a></li>
+                        <li class="divider"></li>
+                        <li><a href="https://eksheba.gov.bd/privacy-policy">গোপনীয়তার নীতিমালা</a></li>
+                        <li class="divider"></li>
+                        <li><a href="https://eksheba.gov.bd/usage-conditions">ব্যবহারের শর্তাবলি</a></li>
+                        <li class="divider"></li>
+                        <li><a data-toggle="modal" style="text-transform:uppercase;" data-target="#faq"> সচরাচর জিজ্ঞাসা</a></li>
+                        <li class="divider"></li>
+                        <li><a data-toggle="modal" data-target="#helpdesk-modal">হেল্প ডেস্ক</a></li>
+                                            
+                    </ul> --}}
+                </nav>
             </div>
-            {{-- <div class="col-12 col-md-3 text-center">
-                <div class="credit">
-                    <div class="credit-title mt-2">
-                     FFWC, BWDB
-                    &copy; 2014 All Rights Reserved
-                   
-                    </div>
-                </div>
-            </div> --}}
-           
-        </div>
     </div>
-{{--     <div class="container copy-right">
-        Copyrights © 2018 All Rights Reserved. a2i, Government of Bangladesh
-    </div> --}}
-
-     <div class="dev-by" style="position: absolute;right: 5px;bottom: 0px;color:white;font-weight: bold;">
-                <div class="developer-corner mt-1">
-                    <div class="developer-title">
-                    </div>
-                    <div class="developer">
-                    	{{-- <span>company logo</span> --}}
-                        
-                    </div>
-                </div>
-      </div>
-{{-- 
-        <a href="" target="_blank">
-            <img src="{{asset('images/riimes.jpg')}}" style="position: absolute;bottom: 0;right: 0;height: 100%;">
-        </a> --}}
+    <div class="inline-block" style="height: 100%;text-align: center;">
+        <span style="color: white;font-weight: bold;">Powered By: </span> 
+        <img src="{{asset('images/logo_rimes_2.png')}}" style="height: 100%;padding: 4px;">
+        <img src="{{asset('images/Islamic Relief.png')}}" style="height: 100%;padding: 4px;"> 
+        <img src="{{asset('images/European Union.png')}}" style="height: 100%;padding: 4px;">
+        <img src="{{asset('images/Concern.png')}}" style="height: 100%;padding: 4px;">
+        <img src="{{asset('images/CARE.png')}}" style="height: 100%;padding: 4px;">
+        <img src="{{asset('images/ASOD.png')}}" style="height: 100%;padding: 4px;">
+    </div>
+    
 </footer>		
